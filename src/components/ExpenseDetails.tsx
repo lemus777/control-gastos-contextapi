411910ed2de6377dd06abdd86a9b5a7f56c24f69
lemus <lemus777@gmail.com>
2024-07-01 20:04:26 +0200

    component for expenses created

---
+++ src/components/ExpenseDetails.tsx
@@ -0,0 +1,12 @@
+import { Expense } from "../types"
+
+type ExpenseDetailsProps = {
+  expense: Expense
+
+}
+
+export default function ExpenseDetails({ expense } : ExpenseDetailsProps) {
+  return (
+    <div>ExpenseDetails</div>
+  )
+}
