bb3bf13669b4945cbaf71686deac0e95c013dd40
lemus <lemus777@gmail.com>
2024-07-02 16:04:10 +0200

    showing expenses details

--- src/components/ExpenseDetails.tsx
+++ src/components/ExpenseDetails.tsx
@@ -1,4 +1,6 @@
+import { formatDate } from "../helpers"
 import { Expense } from "../types"
+import AmountDisplay from "./AmountDisplay"
 
 type ExpenseDetailsProps = {
   expense: Expense
@@ -7,6 +9,15 @@ type ExpenseDetailsProps = {
 
 export default function ExpenseDetails({ expense } : ExpenseDetailsProps) {
   return (
-    <div>ExpenseDetails</div>
+    <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
+      <div></div>
+      <div>
+        <p>{expense.expenseName}</p>
+        <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
+      </div>
+      <AmountDisplay
+        amount={expense.amount}
+      />
+    </div>
   )
 }
