3158afe8e32e8a9d8bce81df7f6f84202e64e060
lemus <lemus777@gmail.com>
2024-07-03 16:10:29 +0200

    edit expense action

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -51,8 +51,13 @@ export default function ExpenseForm() {
       return
     }
     
-    // Agregar un nuevo gasto
-    dispatch({ type: 'add-expense', payload: { expense }})
+    // Agregar o actualizar el gasto
+    if (state.editingId) {
+      dispatch({ type: 'update-expense', payload: { expense: {id: state.editingId, ...expense} }})
+    } else {
+      dispatch({ type: 'add-expense', payload: { expense }})
+    }
+    
 
     // Reiniciar el state
     setExpense({
