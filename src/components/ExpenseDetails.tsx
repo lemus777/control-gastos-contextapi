2b2a6db01b218d57a9d18a9605c2b53fc9340d66
lemus <lemus777@gmail.com>
2024-07-04 20:51:10 +0200

    created a filter by category

--- src/components/ExpenseDetails.tsx
+++ src/components/ExpenseDetails.tsx
@@ -44,7 +44,7 @@ export default function ExpenseDetails({ expense } : ExpenseDetailsProps) {
         leadingActions={leadingActions()}
         trailingActions={trailingActions()}
       >
-        <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
+        <div className="bg-white shadow-lg p-5 w-full border-b border-gray-200 flex gap-5 items-center">
           <div>
             <img
               src={`/icono_${categoryInfo.icon}.svg`}
